c8979d3bb16392a7fe34b70fcb5b4b5663ab6b7c
danielstevenson182 <danielstevenson70@gmail.com>
2026-02-05 21:18:44 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router";
+import { AuthProvider } from "./AuthContext";
+
+import MainLayout from "./layouts/MainLayout";
+import ProtectedLayout from "./layouts/ProtectedLayout";
+
+import Home from "./Pages/home";
+import Login from "./Pages/login";
+import Register from "./Pages/register";
+import Logout from "./Pages/logout";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AuthProvider>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+          <Route element={<ProtectedLayout />}>
+            <Route path="/logout" element={<Logout />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+
+export default App;
+
---
+++ src/pages/Search.jsx
@@ -0,0 +1,12 @@
+//import { useState } from "react";
+//import { useParams, Link } from "react-router-dom";
+
+//const Home = () => {
+//    const [ set] = useState([]);
+//    const[linkName, setLinkName] = useState("")
+
+//const handleSumbit = (e) =>{
+//    e.preventDefault();
+//    const fetch
+//}
+//}
